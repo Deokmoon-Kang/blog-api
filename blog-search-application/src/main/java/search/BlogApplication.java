2bd28b6a8a7d63c700f3e1f8e8134acf3a038980
deokmoon <deok2moon@gmail.com>
2023-03-22 17:57:45 +0900

    feat: main application에 feign 어노테이션 적용

--- blog-search-application/src/main/java/search/BlogApplication.java
+++ blog-search-application/src/main/java/search/BlogApplication.java
@@ -1,8 +1,14 @@
 package search;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
 
+@EnableFeignClients
 @SpringBootApplication
 public class BlogApplication {
 
